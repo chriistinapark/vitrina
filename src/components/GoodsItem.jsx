9a6d956f2d315b2e549a2be0fb5fe00f2c04c5b9
chriistinapark <playful_oddi@mail.ru>
2025-05-19 18:22:20 +0300

    Обновлён стиль корзины: GoodsItem с img

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -1,42 +1,3 @@
-// import React from 'react';
-
-// export function GoodsItem(props) {
-//   const {
-//     mainId: id,
-//     displayName: name,
-//     price: { finalPrice: price } = {},
-//     displayAssets = [],
-//     addToBasket = Function.prototype,
-//   } = props;
-
-//   const img = displayAssets.length ? displayAssets[0].url : 'https://via.placeholder.com/300x300?text=No+image';
-
-//   return (
-//     <div className="card">
-//       <div className="card-image">
-//         <img src={img} alt={name} />
-//       </div>
-//       <div className="card-content">
-//         <span className="card-title">{name}</span>
-//         <p>Цена: {price}</p>
-//       </div>
-//       <div className="card-action">
-//         <button
-//           className="btn deep-purple lighten-1"
-//           onClick={() =>
-//             addToBasket({
-//               id,
-//               name,
-//               price,
-//             })
-//           }
-//         >
-//           Купить
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
 
 import React from 'react';
 
@@ -49,7 +10,7 @@ export function GoodsItem(props) {
     addToBasket = Function.prototype,
   } = props;
 
-  // URL изображения (берём первый asset или заглушку)
+  
   const img = displayAssets.length
     ? displayAssets[0].url
     : 'https://via.placeholder.com/300x300?text=No+image';
@@ -73,7 +34,7 @@ export function GoodsItem(props) {
               id,
               name,
               price,
-              img,        // ← передаём URL изображения в корзину
+              img,        
             })
           }
         >
